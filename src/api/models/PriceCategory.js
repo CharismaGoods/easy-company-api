609ac976fa2c5b217e8928ffdfe5fab7ed8118e5
Husam Burhan <husam.burhan@gmail.com>
2022-01-18 14:40:44 +0300

    Create PriceCategory.js

---
+++ src/api/models/PriceCategory.js
@@ -0,0 +1,10 @@
+/**
+ * long description for the file
+ *
+ * @summary short description for the file
+ * @author Muhammad
+ *
+ * Created at     : 2022-01-18 14:34:50 
+ * Last modified  : 2022-01-18 14:35:07
+ */
+
