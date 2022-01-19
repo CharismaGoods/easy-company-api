fe8ba29673e945139521446bf32337bcc1317251
Husam Burhan <husam.burhan@gmail.com>
2022-01-19 09:15:58 +0300

    remove sub-category features

--- src/api/models/Category.js
+++ src/api/models/Category.js
@@ -1,11 +1,9 @@
 const pool = require('../helpers/mysql');
-const SubCategory = require('./SubCategory');
 
 class Category {
     constructor(id, name) {
         this.id = id;
         this.name = name;
-        this.sub_categories = [];
     }
 
     static getCategoryById = async (id) => {
@@ -57,32 +55,6 @@ class Category {
             }
         }
         catch (err) {            
-            throw err;
-        }
-    }
-
-    static getSubCategories = async (category_id) => {
-        let sql = '';
-        let values = [];        
-
-        try {
-            sql = `SELECT * 
-                        FROM sub_categories 
-                        WHERE category_id  = ?
-                        ORDER BY name;`;
-
-            values.push(`${category_id}`); 
-
-            let results = await pool.query(sql, values);
-
-            if (results.length > 0) {
-                return results.map(r => new SubCategory(r.id, r.name));
-            }
-            else {
-                return null;
-            }
-        }
-        catch (err) {                   
             throw err;
         }
     }
